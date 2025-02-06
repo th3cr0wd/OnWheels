79fb1b8836972cd3ee59bf13840831725a51169b
th3cr0wd <juricka3107@gmail.com>
2025-02-06 18:43:23 +0100

    Implementace tab3 - calculator

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,7 +7,34 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab3Page {
+  distance: number = 0;
+  consumption: number = 0;
+  fuelPrice: number = 0;
+  returnTrip: boolean = false;
 
-  constructor() {}
+  totalDistance: number = 0;
+  totalFuel: number = 0;
+  totalCost: number = 0;
 
+  calculate() {
+    if (this.distance <= 0 || this.consumption <= 0 || this.fuelPrice <= 0) {
+      alert('Vyplňte všechna pole s platnými hodnotami.');
+      return;
+    }
+
+    this.totalDistance = this.returnTrip ? this.distance * 2 : this.distance;
+    this.totalFuel = (this.totalDistance * this.consumption) / 100;
+    this.totalCost = this.totalFuel * this.fuelPrice;
+  }
+
+  reset() {
+    this.distance = 0;
+    this.consumption = 0;
+    this.fuelPrice = 0;
+    this.returnTrip = false;
+
+    this.totalDistance = 0;
+    this.totalFuel = 0;
+    this.totalCost = 0;
+  }
 }
