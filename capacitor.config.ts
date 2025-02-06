6bbbd322b7d6e626fc88eb42cc147f3af95214ff
th3cr0wd <juricka3107@gmail.com>
2025-02-06 22:14:29 +0100

    Added icon and splash screen.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'OnWheels',
   webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Show splash for 3 seconds
+      backgroundColor: "#ffffff", // Change to your preferred color
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
+
